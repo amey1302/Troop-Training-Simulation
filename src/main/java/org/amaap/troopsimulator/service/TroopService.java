39f90c19e90bc9a8508d61030d7ff8cfe3b5ebf3
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-04-07 18:53:13 +0530

    Update class with method view

--- src/main/java/org/amaap/troopsimulator/service/TroopService.java
+++ src/main/java/org/amaap/troopsimulator/service/TroopService.java
@@ -1,9 +1,16 @@
 package org.amaap.troopsimulator.service;
 
+import org.amaap.troopsimulator.domain.ArmyCamp;
+import org.amaap.troopsimulator.domain.model.Archer;
+import org.amaap.troopsimulator.domain.model.Barbarian;
 import org.amaap.troopsimulator.domain.model.TroopType;
 import org.amaap.troopsimulator.domain.service.TrainTroop;
+import org.amaap.troopsimulator.domain.service.ViewTroop;
+import org.amaap.troopsimulator.repository.TroopDatabase;
 import org.amaap.troopsimulator.service.exception.InvalidTroopCountException;
 import org.amaap.troopsimulator.service.exception.InvalidTroopTypeException;
+
+import java.util.LinkedList;
 
 import static org.amaap.troopsimulator.service.validator.TroopTypeValidator.isNotInEnum;
 
@@ -15,5 +22,21 @@ public class TroopService {
             throw new InvalidTroopTypeException("Invalid TroopType Type Exception " + troopType);
 
         return TrainTroop.train(trooperCount, troopType);
+    }
+
+    public static boolean ViewTroopData() {
+
+        ArmyCamp armyCamp=new ArmyCamp();//check once here...dont make any sense
+        ViewTroop viewTroop = new ViewTroop(armyCamp);
+        LinkedList<Barbarian> trainedBarbarians = viewTroop.viewTrainedBarbarians();
+        LinkedList<Archer> trainedArchers = viewTroop.viewTrainedArchers();
+        LinkedList<Object> combinedTroops = viewTroop.viewCombinedTrainedTroops();
+
+        // Store the trained data in the repository
+        TroopDatabase.storeTrainedBarbarians(trainedBarbarians);
+        TroopDatabase.storeTrainedArchers(trainedArchers);
+        TroopDatabase.storeCombinedTrainedTroops(combinedTroops);
+
+        return  true;
     }
 }
